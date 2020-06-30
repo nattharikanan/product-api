5d4cbdbfc5b9ef1825808813038c4938e40a384b
nattharikanan <63855810+nattharikanan@users.noreply.github.com>
2020-06-30 13:44:02 +0700

    Update database.js
    //add clear db

--- config/database.js
+++ config/database.js
@@ -2,10 +2,10 @@
 module.exports = {
   port: 7000,
   db: {
-    host : 'localhost',
-    user : 'root',
-    password : '12345678',
-    database : 'cpac'
+    host : 'us-cdbr-east-02.cleardb.com',
+    user : 'b6fdd4db30c792',
+    password : '80541457',
+    database : 'heroku_6fbeab3a805c0bb'
   },
   socket: {
     url: 'https://socket.bpcd.xenex.io',
